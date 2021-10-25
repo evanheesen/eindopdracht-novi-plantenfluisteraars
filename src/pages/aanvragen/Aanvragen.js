ee5cfa86cf95d529a50cc1220248ab1b32a3838e
evanheesen <evanheesen@gmail.com>
2021-10-25 14:41:11 +0200

    start aanvraagformulier hook forms

--- src/pages/aanvragen/Aanvragen.js
+++ src/pages/aanvragen/Aanvragen.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import ColoredContainer from "../../components/coloredContainer/ColoredContainer";
+import aanlegGeveltuin from "../../assets/aanleg-geveltuin.webp";
+import ImageContainer from "../../components/imageContainer/ImageContainer";
 
-function Aanvragen(props) {
+function Aanvragen() {
     return (
-        <div></div>
+        <>
+        <div className="section--split">
+            <ColoredContainer
+                classNameItem="section-item section-item--split"
+                classNameBlock="block block--left block--green"
+                title="Aanvragen aanleg geveltuin"
+                text="tekst aanvragen geveltuin"
+                buttonClassNameBottom="button button--bottom button--bottom-split"
+                buttonTitleBottom="Ga naar het aanvraagformulier"
+                buttonLinkBottom="/#aanvraagformulier"
+            />
+            <ImageContainer
+                source={aanlegGeveltuin}
+                alt="aanleg-geveltuin"
+                classNameImg="image image--right"
+            />
+            <ColoredContainer
+                classNameItem="section-item section-item--split"
+                classNameBlock="block block--right block--red"
+                title="Planning"
+                text="bullits planning"
+            />
+        </div>
+        </>
     );
 }
 
